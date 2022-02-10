4d147c413e2760dee9bb9461cc13243df033f315
Andrea Marino <am.marinoandrea@gmail.com>
2022-02-10 15:01:26 +0100

    🚧 [assignment1]  sequential work in progress
    Co-authored-by: mullaisanthanam <mullaisanthanam@users.noreply.github.com>
    Co-authored-by: Thomas Dingemanse <contact@thomasdingemanse.nl>

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -1,13 +1,42 @@
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "compute.h"
 
-/// Replace/erase the following line:
-#include "ref1.c"
-
-void do_compute(const struct parameters* p, struct results *r)
+void do_compute(const struct parameters *p, struct results *r)
 {
-/// Replace/erase the following line:
-#include "ref2.c"
+    size_t n_cols = p->M;
+    size_t n_rows = p->N;
+    size_t n_cells = n_rows * n_cols;
+
+    double *m_heat = (double *)alloca(sizeof(double) * (n_cells + 2 * n_cols));
+    double *m_coef = (double *)alloca(sizeof(double) * (n_cells));
+
+    memcpy(&m_heat, &p->tinit, sizeof(double) * (n_cols));
+    memcpy(&m_heat, &p->tinit + n_cols, sizeof(double) * (n_cells));
+    memcpy(&m_heat + (n_cells - n_cols), &p->tinit + (n_cells + n_cols), sizeof(double) * (n_cols));
+    memcpy(&m_coef, &p->conductivity, sizeof(double) * (n_cells));
+
+    double neighbors[8]; // clockwise, starting at the top
+
+    for (size_t i = 0; i < p->maxiter; ++i)
+    {
+        for (size_t row = 1; row < n_rows + 1; ++row)
+        {
+            for (size_t col = 0; col < n_cols; ++col)
+            {
+                double coef = m_coef[row * n_cols + col];
+
+                neighbors[0] = m_heat[(row - 1) * n_cols + col];
+                neighbors[1] = m_heat[(row - 1) * n_cols + col + 1];
+                neighbors[2] = m_heat[row * n_cols + col + 1];
+                neighbors[3] = m_heat[(row + 1) * n_cols + col + 1];
+                neighbors[4] = m_heat[(row + 1) * n_cols + col];
+                neighbors[5] = m_heat[(row + 1) * n_cols + col - 1];
+                neighbors[6] = m_heat[row * n_cols + col - 1];
+                neighbors[7] = m_heat[(row - 1) * n_cols + col - 1];
+            }
+        }
+    }
 }
