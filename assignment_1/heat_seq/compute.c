4fcf109e9c51710c9dd269294df769d257773622
Andrea Marino <am.marinoandrea@gmail.com>
2022-02-10 17:05:35 +0100

    🐛 [assignment1] fixed memcpy halo indices

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -1,7 +1,7 @@
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include "compute.h"
 
 void do_compute(const struct parameters *p, struct results *r)
@@ -9,18 +9,22 @@ void do_compute(const struct parameters *p, struct results *r)
     size_t n_cols = p->M;
     size_t n_rows = p->N;
     size_t n_cells = n_rows * n_cols;
+    size_t n_iters = p->maxiter; 
 
+    // allocating stack space for the matrices
+    // the heat matrix has 2 additional rows for the halo values
     double *m_heat = (double *)alloca(sizeof(double) * (n_cells + 2 * n_cols));
     double *m_coef = (double *)alloca(sizeof(double) * (n_cells));
 
-    memcpy(&m_heat, &p->tinit, sizeof(double) * (n_cols));
-    memcpy(&m_heat, &p->tinit + n_cols, sizeof(double) * (n_cells));
-    memcpy(&m_heat + (n_cells - n_cols), &p->tinit + (n_cells + n_cols), sizeof(double) * (n_cols));
-    memcpy(&m_coef, &p->conductivity, sizeof(double) * (n_cells));
+    // we copy the first and last row of the heat matrix twice for the halo values
+    memcpy(&m_heat,                         &p->tinit,                      sizeof(double) * (n_cols));
+    memcpy(&m_heat + n_cols,                &p->tinit,                      sizeof(double) * (n_cells));
+    memcpy(&m_heat + (n_cells + n_cols),    &p->tinit + (n_cells - n_cols), sizeof(double) * (n_cols));
+    memcpy(&m_coef,                         &p->conductivity,               sizeof(double) * (n_cells));
 
     double neighbors[8]; // clockwise, starting at the top
 
-    for (size_t i = 0; i < p->maxiter; ++i)
+    for (size_t i = 0; i < n_iters; ++i)
     {
         for (size_t row = 1; row < n_rows + 1; ++row)
         {
